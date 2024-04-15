8ea54634c785d24e413b24da8966bccacf4a4efa
unknown <gshubham007@outlook.com>
2024-04-15 22:01:08 +0530

    Predict pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,82 +1,88 @@
 import os
 import sys
 from dataclasses import dataclass
-
-from sklearn.ensemble import(
-    RandomForestRegressor
+from sklearn.ensemble import (
+    RandomForestRegressor,
 )
-
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
+
 from sklearn.tree import DecisionTreeRegressor
+
 
 from src.exception import CustomException
 from src.logger import logging
 
-from src.utils import save_object
-from src.utils import evaluate_models
+from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path=os.path.join("artifacts","model.pkl")
 
-
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
-    
+
+
     def initiate_model_trainer(self,train_array,test_array):
         try:
             logging.info("Split training and test input data")
-            X_train, y_train, X_test, y_test= (
+            X_train,y_train,X_test,y_test=(
                 train_array[:,:-1],
                 train_array[:,-1],
                 test_array[:,:-1],
                 test_array[:,-1]
             )
-
-            models={
+            models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
-                "Linear Regression": LinearRegression()
+                "Linear Regression": LinearRegression(),
             }
-
             params={
                 "Decision Tree": {
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson']
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
                 },
                 "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
                     'n_estimators': [8,16,32,64,128,256]
                 },
-                "Linear Regression":{}   
+                "Linear Regression":{}
             }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,
-                                             y_test=y_test,models=models,params=params)
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,param=params)
             
-            best_model_score=max(sorted(model_report.values()))
+            ## To get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
 
-            best_model_name=list(model_report.keys())[
+            ## To get best model name from dict
+
+            best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-            
             best_model = models[best_model_name]
-
-            logging.info(f"Best model: {best_model}")
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
-
             logging.info(f"Best found model on both training and testing dataset")
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
-                obj=best_model_name
+                obj=best_model
             )
 
             predicted=best_model.predict(X_test)
-            r2score = r2_score(y_test,predicted)
-            return r2score
 
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+            
+
+
+
+            
         except Exception as e:
             raise CustomException(e,sys)
